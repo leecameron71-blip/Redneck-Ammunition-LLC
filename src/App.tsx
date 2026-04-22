a0bccebdb27e656e4e1981c0162c2b1baa7d1487
leecameron71-blip <leecameron71@gmail.com>
2026-04-22 09:42:38 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
       <h1>Redneck Ammunition</h1>
       <p>Built for performance. Designed for control.</p>
       <div style={{ margin: "50px 0" }}>
-  <h2 style={{ marginBottom: "10px"}}>Tactical Rail Protection</h2>
+  <h2 style={{ marginBottom: "8px"}}>Tactical Rail Protection</h2>
 
   <a
     href="https://redneck-ammunition-llc.square.site/product/ar15-rail-cover-set-top-bottom-tactical-rail-protection-redneck-ammunition/BWTD5DJQ2SWVBTE6VCTWDXSO?cp=true&sa=true&sbp=false&q=false"
@@ -25,7 +25,7 @@ export default function App() {
   </a>
 </div>
       <div style={{ margin: "50px 0" }}>
-  <h2 style={{ marginBottom: "10px"}}>Low Profile Angled Grip </h2>
+  <h2 style={{ marginBottom: "8px"}}>Low Profile Angled Grip </h2>
 
   <a
     href="https://redneck-ammunition-llc.square.site/product/m-lok-hand-stop-for-ar15-low-profile-angled-grip-tactical-rail-stop-lightweight-rifle-control-redneck-ammunition/GSUGXR6722XBAZUTYAOOQAFL?cp=true&sa=true&sbp=false&q=false"
@@ -46,7 +46,7 @@ export default function App() {
   </a>
 </div>
       <div style={{ margin: "50px 0" }}>
-  <h2 style={{ marginBottom: "10px"}}>Quick Detach Sling Adapter</h2>
+  <h2 style={{ marginBottom: "8px"}}>Quick Detach Sling Adapter</h2>
 
   <a
     href="https://redneck-ammunition-llc.square.site/product/m-lok-qd-sling-mount-for-ar15-quick-detach-sling-adapter-tactical-sling-attachment-point-low-profile-tough-resin-redneck-ammunition/HJG6TFJXJLVMFDIMXJDHD5S7?cp=true&sa=true&sbp=false&q=false"
@@ -66,7 +66,7 @@ export default function App() {
     Buy Now – $12.99
   </a></div>
 <div style={{ margin: "50px 0" }}>
-  <h2 style={{ marginBottom: "10px"}}>Tactical Rifle Stop</h2>
+  <h2 style={{ marginBottom: "8px"}}>Tactical Rifle Stop</h2>
 
   <a
     href="https://redneck-ammunition-llc.square.site/product/m-lok-barricade-stop-for-ar15-tactical-rifle-stop-precision-shooting-support-low-profile-rail-stop-tough-resin-redneck-ammunition/HO4KRXNUTPAQEVRN63DXABHJ?cp=true&sa=true&sbp=false&q=false"
@@ -87,7 +87,7 @@ export default function App() {
   </a>
 </div>
       <div style={{ margin: "50px 0" }}>
-  <h2 style={{ marginBottom: "10px"}}>Rifle Wall Holder</h2>
+  <h2 style={{ marginBottom: "8px"}}>Rifle Wall Holder</h2>
 
   <a
     href="https://redneck-ammunition-llc.square.site/product/ar15-wall-mount-rifle-wall-holder-m-lok-compatible-gun-mount-tactical-storage-display-space-saving-rifle-rack-redneck-ammunition/L2O5DLPWHHACWX6LYN3DUPPA?cp=true&sa=true&sbp=false&q=false"
@@ -108,7 +108,7 @@ export default function App() {
   </a>
 </div>
 <div style={{ margin: "50px 0" }}>
-  <h2 style={{ marginBottom: "10px"}}>Rifle Stand</h2>
+  <h2 style={{ marginBottom: "8px"}}>Rifle Stand</h2>
   <a
     href="https://redneck-ammunition-llc.square.site/product/ar15-display-stand-rifle-stand-for-ar-platform-gun-display-stand-redneck-ammunition/RGRPRQWONNIJGEB4WJJAYMQF?cp=true&sa=true&sbp=false&q=false"
     target="_blank"
@@ -128,7 +128,7 @@ export default function App() {
   </a>
 </div>
       <div style={{ margin: "50px 0" }}>
-        <h2 style={{ marginBottom: "10px"}}>M-LOK Hand Stop</h2>
+        <h2 style={{ marginBottom: "8px"}}>M-LOK Hand Stop</h2>
         <a
           href="https://redneck-ammunition-llc.square.site/product/m-lok-mounting-kit-ar15-mlok-hardware-set-rail-attachment-system-tactical-accessories-lightweight-durable-5-pack-redneck-ammunition/LESC4JGK4CAZNJWY5KNPENUO?cp=true&sa=true&sbp=false&q=false"
           target="_blank"
